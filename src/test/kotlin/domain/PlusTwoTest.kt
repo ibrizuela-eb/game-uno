707924dd225e51aaef30954b96e26865b2d73780
ibrizuela-eb <ibrizuela@eventbrite.com>
2022-06-06 17:50:57 -0300

    Add test to cards

---
+++ src/test/kotlin/domain/PlusTwoTest.kt
@@ -0,0 +1,16 @@
+package domain
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+//class PlusTwoTest : StringSpec({
+//    "should return the card value representation" {
+//        val plusTwoCard = PlusTwoCard()
+//        plusTwoCard.value shouldBe "+2"
+//    }
+//
+//    "should return the effect of the card" {
+//        val plusTwoCard = PlusTwoCard()
+//        plusTwoCard.effect() shouldBe "take_two_cards"
+//    }
+//})
